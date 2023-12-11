c5301104e3466f20d9f4854b2352c7bdb658d6b0
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2023-12-11 19:53:58 -0300

    primeira configuração da aplicação

---
+++ agro/src/main/java/com/ods/agro/AgroApplication.java
@@ -0,0 +1,13 @@
+package com.ods.agro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroApplication.class, args);
+	}
+
+}
