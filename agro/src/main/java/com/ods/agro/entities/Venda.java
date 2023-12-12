0e7fde62fdb646d722b37eada8c4b54ce54d450f
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2023-12-11 21:03:55 -0300

    implementação cadastro de Venda

---
+++ agro/src/main/java/com/ods/agro/entities/Venda.java
@@ -0,0 +1,30 @@
+package com.ods.agro.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity(name = "venda")
+@Getter
+@Setter
+public class Venda extends EntityID{
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", referencedColumnName = "id")
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id", referencedColumnName = "id")
+    private Produto produto;
+
+    @Column(name = "qtd_produto")
+    private Double qtdProduto;
+
+    @Column(name = "dt_venda")
+    private LocalDate dataVenda;
+}
