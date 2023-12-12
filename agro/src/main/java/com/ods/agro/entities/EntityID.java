3e6fe5dc7c709bbb184e463f5e18cf2877631402
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2023-12-11 21:01:33 -0300

    implementação entityID

---
+++ agro/src/main/java/com/ods/agro/entities/EntityID.java
@@ -0,0 +1,19 @@
+package com.ods.agro.entities;
+
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class EntityID {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+}
